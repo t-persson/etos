dc2996f18f248049a4d87096e5dde58569decd74
Tobias Persson <tobiaspn@axis.com>
2026-02-19 10:54:11 +0100

    Add a wait function

--- scripts/set_rebase_labels.sh
+++ scripts/set_rebase_labels.sh
@@ -3,31 +3,54 @@ set -x
 
 REPOSITORY="t-persson/etos"
 LABEL="automated-rebase"
+TIMEOUT=60  # Timeout in seconds (1 minute)
 
 if [ ! -x "$(command -v gh)" ]; then
   echo "Error: gh CLI is not installed." >&2
   exit 1
 fi
 
-for num in `gh pr list -R $REPOSITORY 2>/dev/null | awk '{print $1}'`; do
-  MERGEABLE=$(gh pr view -R $REPOSITORY $num --json mergeable --jq ".mergeable")
-  echo "PR #$num mergeable status: $MERGEABLE"
-  has_label=$(gh pr view -R $REPOSITORY $num --json labels --jq ".labels | map(select(.name == \"$LABEL\")) | length > 0")
-  if [ "$MERGEABLE" = "CONFLICTING" ]; then
-    echo "PR #$num is not mergeable. Checking for '$LABEL' label..."
-    if [ "$has_label" = "true" ]; then
-      echo "PR #$num has the '$LABEL' label but is not mergeable. Removing label..."
-      gh pr edit -R $REPOSITORY --remove-label "$LABEL" $num
+wait_for_status() {
+  PR_NUM=$1
+  START=$EPOCHSECONDS
+  while true; do
+    MERGEABLE=$(gh pr view -R $REPOSITORY $PR_NUM --json mergeable --jq ".mergeable")
+    if [[ "$MERGEABLE" == "UNKNOWN" ]]; then
+      sleep 5
+      if (( EPOCHSECONDS - START > TIMEOUT )); then
+        exit 1
+      fi
+      continue
     else
-      echo "PR #$num does not have the '$LABEL' label and is not mergeable. No action needed."
+      echo "$MERGEABLE"
+      break
+    fi
+  done
+}
+
+for NUM in `gh pr list -R $REPOSITORY 2>/dev/null | awk '{print $1}'`; do
+  echo "Processing PR #$NUM..."
+  MERGEABLE=$(wait_for_status $NUM)
+  if [ $? -gt 0 ]; then
+    echo "Failed to get mergeable status for PR #$NUM after $TIMEOUT seconds. Skipping."
+    continue
+  fi
+  echo "PR #$NUM mergeable status: $MERGEABLE"
+
+  has_label=$(gh pr view -R $REPOSITORY $NUM --json labels --jq ".labels | map(select(.name == \"$LABEL\")) | length > 0")
+  if [ "$MERGEABLE" = "CONFLICTING" ]; then
+    if [ "$has_label" = "false" ]; then
+      echo "PR #$NUM is conflicting, enable automated rebase"
+      gh pr edit -R $REPOSITORY --add-label "$LABEL" $NUM
+    else
+      echo "PR #$NUM is already marked for rebase. No action needed."
     fi
   else
-    echo "PR #$num is mergeable. Checking for '$LABEL' label..."
-    if [ "$has_label" = "false" ]; then
-      echo "PR #$num does not have the '$LABEL' label but is mergeable. Adding label..."
-      gh pr edit -R $REPOSITORY --add-label "$LABEL" $num
+    if [ "$has_label" = "true" ]; then
+      echo "PR #$NUM does not have conflicts, disable automated rebase"
+      gh pr edit -R $REPOSITORY --remove-label "$LABEL" $NUM
     else
-      echo "PR #$num has the '$LABEL' label and is mergeable. No action needed."
+      echo "PR #$NUM is not conflicting and does not have the rebase label. No action needed."
     fi
   fi
 done
